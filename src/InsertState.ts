da6da6d8bb84a7b9813a05f15ddb2cdaf8a8f4b0
Unknown <jan.kuss@yahoo.de>
2019-03-01 21:18:15 +0100

    ~ Basic insert implementation

--- src/InsertState.ts
+++ src/InsertState.ts
@@ -1,8 +1,19 @@
 import { Row } from "./Row";
 import { Table } from "./TableLike";
 import { TableField, Field } from "./Field";
+
+interface OnDuplicateKeyIgnore {
+  kind: "OnDuplicateKeyIgnore";
+}
+
+interface OnDuplicateKeyUpdate<R extends Row> {
+  kind: "OnDuplicateKeyUpdate";
+  updates: [TableField<R, any>, any][];
+}
+
 export interface InsertState<R extends Row> {
   table: Table<R>;
   fields: TableField<R, any>[];
   values: Field<any>[][];
+  duplicateKey?: OnDuplicateKeyIgnore | OnDuplicateKeyUpdate<R>;
 }
