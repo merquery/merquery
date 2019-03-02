fcc9bc9c064f648fb7bb6c99181ef0b882b5b201
Jan Kuß <jan@kuss.dev>
2019-03-02 20:10:11 +0100

    ~ DSL groupBy returns SelectHavingStep instead of SelectGroupByStep

--- src/SelectGroupByStep.ts
+++ src/SelectGroupByStep.ts
@@ -4,5 +4,5 @@ import { TableField, Field } from "./Field";
 import { SelectHavingStep } from "./SelectHavingStep";
 
 export interface SelectGroupByStep<R extends Row> extends SelectHavingStep<R> {
-  groupBy(...fields: Field<any>[]): SelectGroupByStep<R>;
+  groupBy(...fields: Field<any>[]): SelectHavingStep<R>;
 }
